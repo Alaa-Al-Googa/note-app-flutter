bcbcc941e6702eb7140161bfba2d859adfbd7524
Alaa Al Googa <alaaalgooga@gmail.com>
2025-03-03 17:15:20 +0200

    add all state to file AddNoteState

--- lib/cubits/add_note_cubit/add_note_state.dart
+++ lib/cubits/add_note_cubit/add_note_state.dart
@@ -3,4 +3,10 @@ part of 'add_note_cubit.dart';
 @immutable
 sealed class AddNoteState {}
 
-final class AddNoteInitial extends AddNoteState {}
+final class AddNoteInitialState extends AddNoteState {}
+final class AddNoteLoadingState extends AddNoteState {}
+final class AddNoteSuccessState extends AddNoteState {}
+final class AddNoteFailureState extends AddNoteState {
+  final String errMessage;
+  AddNoteFailureState(this.errMessage);
+}
