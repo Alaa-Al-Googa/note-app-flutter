dd12bc5c54d07d830c2c89e48b5cc8e2a82250eb
Alaa Al Googa <alaaalgooga@gmail.com>
2025-03-03 17:25:18 +0200

    add function to cubit file

--- lib/cubits/add_note_cubit/add_note_cubit.dart
+++ lib/cubits/add_note_cubit/add_note_cubit.dart
@@ -1,10 +1,28 @@
 import 'package:bloc/bloc.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:meta/meta.dart';
+import 'package:note/constants.dart';
+import 'package:note/models/note_model.dart';
 
 part 'add_note_state.dart';
 
 class AddNoteCubit extends Cubit<AddNoteState> {
   AddNoteCubit() : super(AddNoteInitialState());
+  
+  
+  addNote(NoteModel note)async{
+    
+    emit(AddNoteLoadingState());
+    
+    try {
+      var noteBox=Hive.box<NoteModel>(kNoteBox);
+      await noteBox.add(note);
+      emit(AddNoteSuccessState());
+      
+    } catch (e) {
+      emit(AddNoteFailureState(e.toString()));
+    }
+  }
 
 
 
