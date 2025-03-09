c24469bc7bc9fb78fc78a8fc6b80e944b5d10902
Alaa Al Googa <alaaalgooga@gmail.com>
2025-03-09 17:00:19 +0200

    add BlocBuilder to NotesListView

--- lib/views/widgets/notes_list_view.dart
+++ lib/views/widgets/notes_list_view.dart
@@ -9,20 +9,18 @@ class NotesListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<NotesCubit, NotesState>(
+    return BlocBuilder<NotesCubit,NotesState>(
       builder: (context, state) {
         List<NoteModel>notes = BlocProvider.of<NotesCubit>
           (context).notes!;
         return Padding(
-          padding: EdgeInsets.zero,
+          padding: EdgeInsets.symmetric(vertical: 4),
           child: ListView.builder(
             itemCount: notes.length,
+              padding: EdgeInsets.zero,
               itemBuilder: (context, index) {
-                return Padding(
-                  padding: EdgeInsets.symmetric(vertical: 4),
-                  child: CustomNoteItem(
-                    note: notes[index],
-                  ),
+                return CustomNoteItem(
+                  note: notes[index],
                 );
               }
           ),
