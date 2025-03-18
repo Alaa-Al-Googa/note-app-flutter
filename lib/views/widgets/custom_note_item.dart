10f91adc4586053a65a7b8d1b5d763ace0f286ed
Alaa Al Googa <alaaalgooga@gmail.com>
2025-03-18 18:41:12 +0200

    add to formatted to date to add note form

--- lib/views/widgets/custom_note_item.dart
+++ lib/views/widgets/custom_note_item.dart
@@ -18,58 +18,61 @@ class CustomNoteItem extends StatelessWidget {
               return EditNoteView();
             }));
       },
-      child: Container(
-        padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
-        decoration: BoxDecoration(
-          color: Color(
-              note.color
+      child: Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: Container(
+          padding: EdgeInsets.only(top: 24,bottom: 24,left: 16),
+          decoration: BoxDecoration(
+            color: Color(
+                note.color
+            ),
+            borderRadius: BorderRadius.circular(16),
           ),
-          borderRadius: BorderRadius.circular(16),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: Text(
-                note.title,
-                style: TextStyle(
-                  color: Colors.black,
-                  fontSize: 26,
-                ),
-              ),
-              subtitle: Padding(
-                  padding:EdgeInsets.only(
-                    top: 16,
-                    bottom: 16,
-                  ),
-                child: Text(
-                  // note.subTitle,
-                  'ffsfsfsf',
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: Text(
+                  note.title,
                   style: TextStyle(
-                    color: Colors.black.withOpacity(.5),
-                    fontSize: 18
+                    color: Colors.black,
+                    fontSize: 26,
+                  ),
+                ),
+                subtitle: Padding(
+                    padding:EdgeInsets.only(
+                      top: 16,
+                      bottom: 16,
+                    ),
+                  child: Text(
+                    // note.subTitle,
+                    'ffsfsfsf',
+                    style: TextStyle(
+                      color: Colors.black.withOpacity(.5),
+                      fontSize: 18
+                    ),
+                  ),
+                ),
+                trailing: IconButton(
+                    onPressed: (){},
+                    icon:Icon(FontAwesomeIcons.trash,
+                      color: Colors.black,
+                      size: 24,
+                ),
+                ),
+              ),
+              Padding(
+                  padding: EdgeInsets.only(right: 24,),
+                child: Text(
+                   note.date,
+                  style: TextStyle(
+                    color: Colors.black.withOpacity(.4),
+                    fontSize: 16,
                   ),
                 ),
               ),
-              trailing: IconButton(
-                  onPressed: (){},
-                  icon:Icon(FontAwesomeIcons.trash,
-                    color: Colors.black,
-                    size: 24,
-              ),
-              ),
-            ),
-            Padding(
-                padding: EdgeInsets.only(right: 24,),
-              child: Text(
-                 note.date,
-                style: TextStyle(
-                  color: Colors.black.withOpacity(.4),
-                  fontSize: 16,
-                ),
-              ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
